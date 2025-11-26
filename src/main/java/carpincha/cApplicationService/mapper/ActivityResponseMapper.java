1bad0928c403ad488f1afb66714d00ca961ec892
Lucy C. <you@example.com>
2025-11-26 16:56:58 +0100

    feat: add actResponseMapper

---
+++ src/main/java/carpincha/cApplicationService/mapper/ActivityResponseMapper.java
@@ -0,0 +1,33 @@
+package carpincha.cApplicationService.mapper;
+
+import carpincha.aCore.entity.activity.Activity;
+import carpincha.cApplicationService.dto.activity.response.ActivityResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ActivityResponseMapper {
+
+    public ActivityResponse toResponse(Activity activity) {
+        return new ActivityResponse(
+                activity.getId(),
+                activity.getTitle(),
+                activity.getDescription(),
+                activity.getCategory(),
+                activity.getFrequency(),
+                activity.getStatus(),
+                activity.getPriority(),
+                activity.getCreatedAt(),
+                activity.getEstimatedDuration(),
+                activity.getDueMoment(),
+                activity.getCompletedAt()
+        );
+    }
+
+    public List<ActivityResponse> toResponseList(List<Activity> activities) {
+        return activities.stream()
+                .map(this::toResponse)
+                .toList();
+    }
+}

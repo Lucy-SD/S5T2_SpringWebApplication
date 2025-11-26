68b71a1f90d9059a25dc3efd931a1e78ab717cd3
Lucy C. <you@example.com>
2025-11-26 16:44:58 +0100

    add activityResponse record

---
+++ src/main/java/carpincha/cApplicationService/dto/activity/response/ActivityResponse.java
@@ -0,0 +1,24 @@
+package carpincha.cApplicationService.dto.activity.response;
+
+import carpincha.aCore.valueObject.ActivityStatus;
+import carpincha.aCore.valueObject.CategoryType;
+import carpincha.aCore.valueObject.FrequencyType;
+import carpincha.aCore.valueObject.PriorityLevel;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public record ActivityResponse(
+        Long id,
+        String title,
+        String description,
+        CategoryType category,
+        FrequencyType frequency,
+        ActivityStatus status,
+        PriorityLevel priority,
+        Instant createdAt,
+        Duration estimatedDuration,
+        Instant dueMoment,
+        Instant completedAt
+) {
+}
